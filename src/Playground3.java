aad97fef339a6695fc3697a169efbc1d49109199
Leon <hualiang.tian@gmail.com>
2022-04-21 21:08:00 -0400

    Practicing with java

--- src/Playground3.java
+++ src/Playground3.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
 public class Playground3 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to a boring world");
+        System.out.print("Please enter your height in cm: ");
+        int height = scanner.nextInt();
+
+        if (height < 130){
+            System.out.println("Your height is below average");
+        }
+        else if (height <= 150){
+            System.out.println("Your height is about average");
+        }
+        else{
+            System.out.println("Holy Molly, you are so tall");
+        }
+    }
 }
