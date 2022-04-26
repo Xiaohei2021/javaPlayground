6e5b6da2669c2bbd86c260a229964d89a557206e
Leon <hualiang.tian@gmail.com>
2022-04-25 22:47:31 -0400

    Practicing with java with array and for loops

---
+++ src/Playground7.java
@@ -0,0 +1,13 @@
+public class Playground7 {
+    public static void main (String[] args){
+        int[] a = {15,29,36,41,53, 90, 57};
+        System.out.println(a[3]);
+        a[3]=43;
+        System.out.println(a[3]);
+
+        for(int i = 0 ; i< 5; i++){
+            System.out.println(a[i]);
+        }
+    }
+
+}
