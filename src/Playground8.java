91ca01995c55d298ef50d3a6bd5b41250b69353a
Leon <hualiang.tian@gmail.com>
2022-04-25 23:31:01 -0400

    Practicing with java + arrays and for loop + count and additon + mean

---
+++ src/Playground8.java
@@ -0,0 +1,23 @@
+public class Playground8 {
+    public static void main(String[] args){
+        /*double[] foo = {0,0,0};
+        double[] bar = new double[3];
+        double[] foobar = {0.6, 22.81, 87.555};
+
+        bar[0]= 98.1;
+        bar[1]= 56.3;
+        bar[2]= 13.69;
+
+        System.out.println(bar[1]);*/
+
+        String[] words = new String[3];
+        words[0] = "woot";
+        words[1] = "yay";
+        words[2] = "hooray";
+
+        System.out.println(words[1]);
+
+
+    }
+
+}
