ab45b883926d8b51bd4aa80f0dcf47ae22c07eda
Leon <hualiang.tian@gmail.com>
2022-04-25 23:22:20 -0400

    Practicing with java + arrays and for loop + count and additon + mean

--- src/Playground7.java
+++ src/Playground7.java
@@ -1,23 +1,44 @@
 public class Playground7 {
     public static void main (String[] args){
 
-        int[] numbs = {15,29,36,41,53, 90, 57, 20, 35, 41};
+        int[] numbs = {15,29,368,41,53, 906, 570, 20, 35, 41};
 //        System.out.println(numbs[3]);
 //
 //        numbs[3]=43;
 //        System.out.println(numbs[3]);
+        int count = 0;
+        double sum = 0;
 
-//        for(int i = 0 ; i< a.length; i++){
-//            System.out.println(numbs[i]);
-//        }
+        for(int i = 0 ; i< numbs.length; i++){
+            /*if (numbs[i] > 100){
+                System.out.println(numbs[i]);
+                System.out.println("array indexes = " + i);
+                System.out.println("array indexes = " + count);
+            }*/
 
-//        for(int i = 9; i>= 0; i--){
-//            System.out.println(numbs[i]);
-//        }
+            /*if (numbs[i] > 100){
+                count++;
+                System.out.println("The number " + numbs[i] + " is greate than 10, at array index of #" + i);
+            }*/
+
+            if (numbs[i] > 100){
+                count++;
+            }
+            sum = sum + numbs[i];
+        }
+        System.out.println("There are " + count + " numbers that is greater than 100.");
+        System.out.println("The sum all of the numbers in the array is " + sum);
+        System.out.println("The mean of the numbers in the array is " + sum/numbs.length );
+
+
+
+        /*for(int i = 9; i>= 0; i--){
+            System.out.println(numbs[i]);
+        }
 
         for(int i = numbs.length - 1; i >= 0; i--) {
             System.out.println(numbs[i]);
-        }
+        }*/
     }
 
 }
