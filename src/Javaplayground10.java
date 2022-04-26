59d1d0d1b0df8da68f97aef075daec07a6679f28
Leon <hualiang.tian@gmail.com>
2022-04-26 12:11:00 -0400

    Practicing with java: Array + for each loop logic

---
+++ src/Javaplayground10.java
@@ -0,0 +1,21 @@
+public class Javaplayground10 {
+    public static void main(String[] args){
+        double[] array1 = new double[]{ 25.92, 1.34, -76.24, 833.11, 76.084};
+        String[] planets = new String[]{"earth", "moon", "sun", "jupiter", "mercury", "mars" };
+
+        /*for (int i = 0; i < array1.length; i++){
+            double e = array1[i];
+            System.out.println(e);
+        }*/
+
+        for (double e : array1){
+            System.out.println(e);
+
+        }
+
+        for (String e : planets){
+            System.out.println(e);
+        }
+
+    }
+}
