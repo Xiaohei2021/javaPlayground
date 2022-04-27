4d72d8332ba7ae805e2ed4e255ca2a5af117eef1
Leon <hualiang.tian@gmail.com>
2022-04-26 22:10:14 -0400

    Practicing with java metod

---
+++ src/Playground11.java
@@ -0,0 +1,20 @@
+public class Playground11 {
+    public static void main(String[] args){
+        System.out.println(19);
+        foo();
+        System.out.println(24);
+        bar();
+
+    }
+    public static void foo(){
+        int x =50;
+        System.out.println(30);
+        System.out.println(16);
+        bar();
+    }
+
+    public static void bar (){
+        System.out.println(12);
+    }
+
+}
