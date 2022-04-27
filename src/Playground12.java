fa4a427e4b4c1a57a854e3d9a731af195cfe4115
Leon <hualiang.tian@gmail.com>
2022-04-26 22:19:18 -0400

    Practicing with java method and parameters

---
+++ src/Playground12.java
@@ -0,0 +1,13 @@
+public class Playground12 {
+    public static void main (String[] args){
+        int var;
+        var = 5;
+//        foo(var);
+        foo(var,56.8,"cake");
+    }
+
+    public static void foo (int a, double b, String c){
+        System.out.println(a + b + c);
+
+    }
+}
