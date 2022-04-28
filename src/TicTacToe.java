205c29cb1a8684e67395d210250704eed41f2dbf
Leon <hualiang.tian@gmail.com>
2022-04-27 23:35:50 -0400

    Practicing with java, tinkering with a different logic for the  tic tac toe game

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -62,11 +62,28 @@ public class TicTacToe {
         public static void main (String[] args) {
 
             Scanner scanner = new Scanner(System.in);
+            char[] pos = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+            char symbol;
             int input;
 
             System.out.println("Hey there, lets play a fun tic tac toe game!!");
+            System.out.println("Which symbol would you like use, X or O?");
+            symbol = scanner.next().charAt(0);
+//            System.out.println(symbol);
 
-            char[] pos = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+            if (symbol != 'X' || symbol!= 'O'){
+                do {
+                    System.out.println("In correct, input. Please only enter 'X' or 'O'.");
+                    System.out.println("capital X or capital O");
+                } while (symbol != 'X' || symbol != 'O');
+            }
+            else{
+                System.out.println("Nice, you have chosen to be " + symbol);
+            }
+
+
+
+
             char turn = 'X';
 //            int currentTurn = 1;
 
@@ -79,33 +96,33 @@ public class TicTacToe {
             System.out.println("---+---+---");
             System.out.println(" 7 | 8 | 9 ");
 
-            for (int i = 0; i < pos.length; i++){
-                while(i < 9 ){
-                    System.out.println("Please refer to the board above and pick a number/position to place your token");
-                    input = scanner.nextInt();
-
-                    pos[input - 1] = turn;
-
-                    System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2] + " ");
-                    System.out.println("---+---+---");
-                    System.out.println(" " + pos[3] + " | " + pos[4] + " | " + pos[5] + " ");
-                    System.out.println("---+---+---");
-                    System.out.println(" " + pos[6] + " | " + pos[7] + " | " + pos[8] + " ");
-
-                    if (pos[0] == turn && pos[1] == turn && pos[2] == turn ||
-                            pos[3] == turn && pos[4] == turn && pos[5] == turn ||
-                            pos[6] == turn && pos[7] == turn && pos[8] == turn ||
-                            pos[0] == turn && pos[3] == turn && pos[6] == turn ||
-                            pos[1] == turn && pos[4] == turn && pos[7] == turn ||
-                            pos[2] == turn && pos[5] == turn && pos[8] == turn ||
-                            pos[0] == turn && pos[4] == turn && pos[8] == turn ||
-                            pos[2] == turn && pos[4] == turn && pos[6] == turn) {
-
-                    }
-
-                }
-
-            }
+//            for (int i = 0; i < pos.length; i++){
+//                while(i < 9 ){
+//                    System.out.println("Please refer to the board above and pick a number/position to place your token");
+//                    input = scanner.nextInt();
+//
+//                    pos[input - 1] = turn;
+//
+//                    System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2] + " ");
+//                    System.out.println("---+---+---");
+//                    System.out.println(" " + pos[3] + " | " + pos[4] + " | " + pos[5] + " ");
+//                    System.out.println("---+---+---");
+//                    System.out.println(" " + pos[6] + " | " + pos[7] + " | " + pos[8] + " ");
+//
+//                    if (pos[0] == turn && pos[1] == turn && pos[2] == turn ||
+//                            pos[3] == turn && pos[4] == turn && pos[5] == turn ||
+//                            pos[6] == turn && pos[7] == turn && pos[8] == turn ||
+//                            pos[0] == turn && pos[3] == turn && pos[6] == turn ||
+//                            pos[1] == turn && pos[4] == turn && pos[7] == turn ||
+//                            pos[2] == turn && pos[5] == turn && pos[8] == turn ||
+//                            pos[0] == turn && pos[4] == turn && pos[8] == turn ||
+//                            pos[2] == turn && pos[4] == turn && pos[6] == turn) {
+//
+//                    }
+//
+//                }
+//
+//            }
 
 //            do {
 //                System.out.println("Please refer to the board above and pick a number/position to place your token");
