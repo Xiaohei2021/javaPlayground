562f72ae4abac4c2d7dac75a1fe0d43d53cda6e1
Leon <hualiang.tian@gmail.com>
2022-04-28 23:54:18 -0400

    Tweaking do while loop  to limit the number of moves to 9.

--- src/TicTacToe2.java
+++ src/TicTacToe2.java
@@ -8,7 +8,6 @@ public class TicTacToe2 {
         char symbol;
         int move;
 
-
         System.out.println("Hey there, lets play a fun tic tac toe game!!");
         System.out.println("Which symbol would you like use, X or O?");
         symbol = scanner.next().charAt(0);
@@ -24,7 +23,7 @@ public class TicTacToe2 {
             /*if (!(symbol == 'X' || symbol == 'O')){
 
                 do {
-                    System.out.println("In correct, input. Please only enter 'X' or 'O'.");
+                    System.out.println("Incorrect input. Please only enter 'X' or 'O'.");
                     System.out.println("capital X or capital O");
                     symbol = scanner.next().charAt(0);
                 } while (!(symbol == 'X' || symbol == 'O'));
@@ -50,11 +49,21 @@ public class TicTacToe2 {
 
         do {
             System.out.println("Please refer to the board above and pick a number/position between 1 -9 and place your token");
-            input = scanner.nextInt();
-            if () {
+            move = scanner.nextInt();
 
+            if (move < 1 || move > 9) {
+                System.out.println("Invalid input， please enter a number/position that is between 1-9");
+                move = scanner.nextInt();
             }
-        }while( move <= 9 );
+
+            System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2] + " ");
+            System.out.println("---+---+---");
+            System.out.println(" " + pos[3] + " | " + pos[4] + " | " + pos[5] + " ");
+            System.out.println("---+---+---");
+            System.out.println(" " + pos[6] + " | " + pos[7] + " | " + pos[8] + " ");
+
+
+        }while( move <= pos.length );
 
        /*for (int i = 0; i < pos.length; i++){
             while(i < 9 ){
@@ -63,11 +72,7 @@ public class TicTacToe2 {
 
                 pos[input - 1] = turn;
 
-                System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2] + " ");
-                System.out.println("---+---+---");
-                System.out.println(" " + pos[3] + " | " + pos[4] + " | " + pos[5] + " ");
-                System.out.println("---+---+---");
-                System.out.println(" " + pos[6] + " | " + pos[7] + " | " + pos[8] + " ");
+
 
                 if (pos[0] == turn && pos[1] == turn && pos[2] == turn ||
                         pos[3] == turn && pos[4] == turn && pos[5] == turn ||
