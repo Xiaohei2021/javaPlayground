3b271a3a56b70e3855b9a226e70d4f29da5c15b9
Leon <hualiang.tian@gmail.com>
2022-04-29 00:08:10 -0400

    commenting do while loop, gives false negative during first pick.

--- src/TicTacToe2.java
+++ src/TicTacToe2.java
@@ -7,30 +7,29 @@ public class TicTacToe2 {
         char[] pos = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
         char symbol;
         int move;
-
         System.out.println("Hey there, lets play a fun tic tac toe game!!");
         System.out.println("Which symbol would you like use, X or O?");
         symbol = scanner.next().charAt(0);
 
-        do {
-            System.out.println("Incorrect input. Please only enter 'X' or 'O'.");
-            System.out.println("capital X or capital O");
-            symbol = scanner.next().charAt(0);
-        } while (!(symbol == 'X' || symbol == 'O'));
+//        do {
+//            System.out.println("Incorrect input. Please only enter 'X' or 'O'.");
+//            System.out.println("capital X or capital O");
+//            symbol = scanner.next().charAt(0);
+//        } while (!(symbol == 'X' || symbol == 'O'));
+//        System.out.println("Nice, you have chosen to be " + symbol + ". You will be making the first move. \n");
 
-        System.out.println("Nice, you have chosen to be " + symbol + ". You will be making the first move.");
-
-            /*if (!(symbol == 'X' || symbol == 'O')){
+            if (!(symbol == 'X' || symbol == 'O')){
 
                 do {
                     System.out.println("Incorrect input. Please only enter 'X' or 'O'.");
                     System.out.println("capital X or capital O");
                     symbol = scanner.next().charAt(0);
                 } while (!(symbol == 'X' || symbol == 'O'));
+                System.out.println("Nice, you have chosen to be " + symbol + ". You will be making the first move. \n");
             }
             else{
-                System.out.println("Nice, you have chosen to be " + symbol);
-            }*/
+                System.out.println("Nice, you have chosen to be " + symbol + ". You will be making the first move. \n");
+            }
 
 //            int currentTurn = 1;
 
@@ -47,23 +46,23 @@ public class TicTacToe2 {
 //
 //        }
 
-        do {
-            System.out.println("Please refer to the board above and pick a number/position between 1 -9 and place your token");
-            move = scanner.nextInt();
-
-            if (move < 1 || move > 9) {
-                System.out.println("Invalid input， please enter a number/position that is between 1-9");
-                move = scanner.nextInt();
-            }
-
-            System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2] + " ");
-            System.out.println("---+---+---");
-            System.out.println(" " + pos[3] + " | " + pos[4] + " | " + pos[5] + " ");
-            System.out.println("---+---+---");
-            System.out.println(" " + pos[6] + " | " + pos[7] + " | " + pos[8] + " ");
-
-
-        }while( move <= pos.length );
+//        do {
+//            System.out.println("Please refer to the board above and pick a number/position between 1 -9 and place your token");
+//            move = scanner.nextInt();
+//
+//            if (move < 1 || move > 9) {
+//                System.out.println("Invalid input， please enter a number/position that is between 1-9");
+//                move = scanner.nextInt();
+//            }
+//
+//            System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2] + " ");
+//            System.out.println("---+---+---");
+//            System.out.println(" " + pos[3] + " | " + pos[4] + " | " + pos[5] + " ");
+//            System.out.println("---+---+---");
+//            System.out.println(" " + pos[6] + " | " + pos[7] + " | " + pos[8] + " ");
+//
+//
+//        }while( move <= pos.length );
 
        /*for (int i = 0; i < pos.length; i++){
             while(i < 9 ){
