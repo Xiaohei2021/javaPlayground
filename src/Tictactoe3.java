7f5d5b65f426a93ae1e0d0580b9ee501fd79c2ad
Leon <hualiang.tian@gmail.com>
2022-05-03 22:18:25 -0400

    Playing around with for loop and prime numbers

--- src/Tictactoe3.java
+++ src/Tictactoe3.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Tictactoe3 {
     public static void main (String[] args){
-        char[] pos = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
+        char[] pos = {' ',  ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
         char token;
         int gridPos;
         int turn =1;
@@ -17,7 +17,7 @@ public class Tictactoe3 {
        }
 
         System.out.println("Perfect, you have chosen the " + token + " Token.");
-        System.out.println("You will be making the first move, please refer to the board below and place your token\n\n ");
+        System.out.println("You will be making the first move, please refer to the board below and place your token\n ");
         System.out.println(" 1 | 2 | 3 ");
         System.out.println("---+---+---");
         System.out.println(" 4 | 5 | 6 ");
@@ -26,23 +26,22 @@ public class Tictactoe3 {
         System.out.println();
         gridPos = scanner.nextInt();
 
+        while(gridPos <1 || gridPos >10){
+            System.out.println("Invalid grid position, please refer to the above grid and ente a position between 1-9");
+            gridPos = scanner.nextInt();
+        }
 
+        if(pos[gridPos-1] =='X'|| pos[gridPos-1]=='O'){
+            System.out.println("This grid position is already taken, please pick another");
+            gridPos = scanner.nextInt();
+        }
+
+        pos[gridPos-1]=token;
+        turn ++;
 
         while(turn < 10){
 
-            while(gridPos <1 || gridPos >10){
-                System.out.println("Invalid grid position, please refer to the above grid and ente a position between 1-9");
-                gridPos = scanner.nextInt();
-            }
 
-            if(pos[gridPos-1] =='X'|| pos[gridPos-1]=='O'){
-                System.out.println("This grid position is already taken, please pick another");
-                gridPos = scanner.nextInt();
-            }
-
-            pos[gridPos-1]=token;
-
-            turn ++;
 
             System.out.println(" " + pos[0] + " | " + pos[1] + " | " + pos[2] + " ");
             System.out.println("---+---+---");
