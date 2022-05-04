df6a9ec68e3ad1631f7195e2b2506143c9bbec29
Leon <hualiang.tian@gmail.com>
2022-05-03 23:38:39 -0400

    Seperated initial code with dried out code, saved old code in MenuPlayground1 for reference and new dried out in  MenuPlayground2

--- src/MenuPlayground1.java
+++ src/MenuPlayground1.java
@@ -2,52 +2,44 @@ import java.util.Scanner;
 
 public class MenuPlayground1 {
 
-    static String [] guests = new String[10];
-    static Scanner scanner = new Scanner(System.in);
+
 
     public static void main(String[] args){
+        String [] guests = new String[10];
+        Scanner scanner = new Scanner(System.in);
 
         do{
-            displayGuests();
-            displayMenu();
-            int option = getOptions();
+            System.out.println("-------------------------------\n - Guests -\n");
+            for (int i= 0; i < guests.length; i++) {
+                System.out.println(guests[i] == null ? "--" : guests[i]);
+            }
 
-//            System.out.println("-------------------------------\n - Guests -\n");
-//            for (int i= 0; i < guests.length; i++) {
-//                System.out.println(guests[i] == null ? "--" : guests[i]);
-//            }
+            System.out.println("-------------------------------\n - Menu -\n");
+            System.out.println();
+//            System.out.println("1 - Display All Guests");
+            System.out.println("1 - Add Guest");
+            System.out.println("2 - Remove Guest");
+            System.out.println("3 - Exit");
+            System.out.print("option: ");
+            int option = scanner.nextInt();
+            System.out.println();
+            System.out.println();
 
-//            System.out.println("-------------------------------\n - Menu -\n");
-//            System.out.println();
-////            System.out.println("1 - Display All Guests");
-//            System.out.println("1 - Add Guest");
-//            System.out.println("2 - Remove Guest");
-//            System.out.println("3 - Exit");
-//            System.out.print("option: ");
-//            int option = scanner.nextInt();
-//            System.out.println();
-//            System.out.println();
+            if (option == 1 ){
+                System.out.println("-------------------------------\n - Guests -\n");
 
-//            System.out.print("option: ");
-//            int option = scanner.nextInt();
-//            System.out.println();
-//            System.out.println();
+                for (int i= 0; i < guests.length; i++){
+//                    if(guests[i] == null){
+//                        guests[i]= "--";
+//                        System.out.println(guests[i]);
+//                    }
+//                    else{
+//                        System.out.println(guests[i]);
+//                    }
+                    System.out.println(guests[i] == null? "--": guests[i]);
 
-//            if (option == 1 ){
-//                System.out.println("-------------------------------\n - Guests -\n");
-//
-//                for (int i= 0; i < guests.length; i++){
-////                    if(guests[i] == null){
-////                        guests[i]= "--";
-////                        System.out.println(guests[i]);
-////                    }
-////                    else{
-////                        System.out.println(guests[i]);
-////                    }
-//                    System.out.println(guests[i] == null? "--": guests[i]);
-//
-//                }
-//            }
+                }
+            }
              if (option == 1) {
                 for(int i=0; i< guests.length; i++){
                     if(guests[i] == null){
@@ -75,30 +67,6 @@ public class MenuPlayground1 {
 
         System.out.println("");
         System.out.println("Exited");
-    }
-
-     static void displayGuests(){
-         System.out.println("-------------------------------\n - Guests -\n");
-         for (int i= 0; i < guests.length; i++) {
-             System.out.println(guests[i] == null ? "--" : guests[i]);
-         }
-    }
-
-    static void displayMenu(){
-        System.out.println("-------------------------------\n - Menu -\n");
-        System.out.println();
-//            System.out.println("1 - Display All Guests");
-        System.out.println("1 - Add Guest");
-        System.out.println("2 - Remove Guest");
-        System.out.println("3 - Exit");
-    }
-
-    static int getOptions(){
-        System.out.print("option: ");
-        int option = scanner.nextInt();
-        System.out.println();
-        System.out.println();
-        return option;
     }
 
 }
